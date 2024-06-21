b4d42245a5f5512913d13d03c752290bf0bb1c29
kourosh-H <kourosh.hajivand0@gmail.com>
2024-06-21 17:45:31 +0330

    update

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,16 +1,23 @@
 "use client"
 import { Canvas } from '@react-three/fiber'
-import React from 'react'
+import React, { Suspense } from 'react'
 import Experince from './Experince'
+import { Html, useProgress } from '@react-three/drei'
 
 function Hero() {
+    function Loader() {
+        const { progress, active } = useProgress()
+        return <Html center><p className='text-white font-bold whitespace-nowrap'>{progress.toFixed(1)} % Loaded</p></Html>
+    }
 
     return (
         <Canvas camera={{
             fov: 70,
             position: [2.3, 1.5, 2.3]
         }}>
-            <Experince />
+            <Suspense fallback={<Loader />}>
+                <Experince />
+            </Suspense>
 
         </Canvas>
     )
